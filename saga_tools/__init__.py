188df1881e7408651bb22807fc2f7ff916c568fa
Jacob L <jalichtefeld@gmail.com>
2020-02-27 16:40:41 -0500

    Move slurm functions into slurm.py

--- saga_tools/__init__.py
+++ saga_tools/__init__.py
@@ -1,18 +1,1 @@
-# pylint:disable=missing-docstring
-from immutablecollections import immutabledict
-from vistautils.memory_amount import MemoryAmount, MemoryUnit
-
 from saga_tools.version import version as __version__  # noqa
-
-SLURM_MEMORY_UNITS = immutabledict(
-    [
-        (MemoryUnit.KILOBYTES, "K"),
-        (MemoryUnit.MEGABYTES, "M"),
-        (MemoryUnit.GIGABYTES, "G"),
-        (MemoryUnit.TERABYTES, "T"),
-    ]
-)
-
-
-def to_slurm_memory_string(memory_request: MemoryAmount) -> str:
-    return f"{memory_request.amount}" f"{SLURM_MEMORY_UNITS[memory_request.unit]}"
